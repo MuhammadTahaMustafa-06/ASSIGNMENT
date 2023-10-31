d254098270d1d4edfe06dd6ed15fb641b4045896
MuhammadTahaMustafa-06 <k228735@nu.edu.pk>
2023-10-31 11:17:11 +0500

    First Commit

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def Calculator(a,b,oper):
+    if oper=='+':
+        return a+b
+    elif oper=="-":
+        return a-b
+    elif oper=="*":
+        return a/b
+    elif oper=="/":
+        return float(a)/float(b)
+print(Calculator(5,6,"/"))
